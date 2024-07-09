ab95db761e81cb774ea8dc76c1034411acf94cb0
mrpablos <141761312+mrpablos@users.noreply.github.com>
2024-07-09 20:10:41 +0300

    Инициализация готового проекта

---
+++ src/components/Input.vue
@@ -0,0 +1,77 @@
+<template>
+  <input
+    min="0"
+    type="number"
+    @input="changeAmount($event.target.value)"
+    placeholder="Введите число"
+  />
+  {{ setCryptoFirst }}
+  {{ setCryptoSecond }}
+  <br />
+  <button
+    @click="convert()"
+    :disabled="setCryptoFirst == '' || setCryptoSecond == ''"
+  >
+    Конвертировать
+  </button>
+  <button @click="favourite()" class="favButton">В избранное</button>
+</template>
+
+<script>
+export default {
+  props: {
+    changeAmount: {
+      type: Function,
+      required: true,
+    },
+    convert: {
+      type: Function,
+      required: true,
+    },
+    favourite: {
+      type: Function,
+      required: true,
+    },
+    setCryptoFirst: {
+      type: String,
+      required: true,
+    },
+    setCryptoSecond: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+input {
+  outline: none;
+  border-radius: 4px;
+  border: 4px solid #1a032d;
+  padding: 10px 15px;
+  background: #fafafa;
+  color: #333;
+  width: 500px;
+  position: relative;
+  top: -50px;
+  font-size: 1.2em;
+}
+
+button {
+  position: relative;
+  top: -20px;
+  margin: auto;
+  padding: 15px 20px;
+  color: #fff;
+  text-transform: uppercase;
+  cursor: pointer;
+  background: #1a032d;
+  border: 0;
+  border-radius: 3px;
+}
+
+button.favButton {
+  margin-left: 20px;
+}
+</style>
---
+++ src/components/Selector.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul>
+    <li @click="setCrypto('BTC')" :class="cryptoNow == 'BTC' ? 'active' : ''">
+      Bitcoin
+    </li>
+    <li @click="setCrypto('ETH')" :class="cryptoNow == 'ETH' ? 'active' : ''">
+      ETH
+    </li>
+    <li @click="setCrypto('USDT')" :class="cryptoNow == 'USDT' ? 'active' : ''">
+      USDT
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    setCrypto: {
+      type: Function,
+      required: true,
+    },
+    cryptoNow: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  width: 200px;
+  padding: 0;
+  border-radius: 3px;
+  border: 3px solid #000;
+}
+
+li {
+  display: block;
+  background: #1a032d;
+  color: #fff;
+  padding: 20px 0;
+}
+
+li:hover,
+li.active {
+  background: #24043e;
+  cursor: pointer;
+}
+</style>
